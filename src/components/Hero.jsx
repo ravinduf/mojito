806b2217566b3483510c70eace4b6868201d2963
ravinduf <ravinduf@rocketmail.com>
2026-03-15 02:02:12 +0530

    basic hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+
+
+const Hero = () => {
+  return (
+    <section id="hero" className="noisy">
+      <h1 className="title">Mojito</h1>
+
+      <img src="/images/hero-left-leaf.png" className="left-leaf" />
+      <img src="/images/hero-right-leaf.png" className="right-leaf" />
+    </section>
+  )
+}
+
+export default Hero

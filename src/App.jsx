3759ca660a460441b48ab300dbc391d74cb01ddd
ravinduf <ravinduf@rocketmail.com>
2026-03-18 23:08:00 +0530

    implemented cocktails sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap  from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
 
 // register plugins to use in the project
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -11,6 +12,7 @@ const App = () => {
    <main>
     <Navbar />
     <Hero />
+    <Cocktails />
    </main>
   )
 }
